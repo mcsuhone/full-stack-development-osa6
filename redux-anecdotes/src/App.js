2592a09379c0772565b061885411aff330e5b977
mcsuhone <mikko.e.suhonen@aalto.fi>
2023-05-15 11:30:19 +0300

    6.9.

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,11 +1,13 @@
 
 import AnecdoteList from './components/AnecdoteList.js'
 import AnecdoteForm from './components/AnecdoteForm.js'
+import Filter from './components/Filter.js'
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
