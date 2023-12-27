95ea5f916e45d4b21f2a9336b63eff1d318fe5f5
mcsuhone <mikko.e.suhonen@gmail.com>
2023-12-27 17:22:21 +0200

    6.22.

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,20 +1,25 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes, createAnecdote } from './services/requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './services/requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const updateAnecdoteMutation = useMutation({mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    }
+  })
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes + 1})
   }
 
   const result = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdotes
   })
-
-  console.log(result)
 
   if (result.isError) {
     return <p>anecdote service not available due to problems in server</p>
--- query-anecdotes/src/services/requests.js
+++ query-anecdotes/src/services/requests.js
@@ -9,4 +9,7 @@ export const createAnecdote = async(anecdote) => {
   console.log(anecdote)
   return await axios.post(baseUrl, anecdote).then(res => res.data)
 }
-  
+
+export const updateAnecdote = async(updatedAnecdote) => {
+  return await axios.put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote).then(res => res.data)
+}
