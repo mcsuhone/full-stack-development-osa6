f61f05f48c72f9541fd98cc7884874aba20d0c26
mcsuhone <mikko.e.suhonen@gmail.com>
2023-12-27 16:29:56 +0200

    6.20.

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,5 +1,7 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './services/requests'
 
 const App = () => {
 
@@ -7,13 +9,20 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes
+  })
+
+  console.log(result)
+
+  if (result.isError) {
+    return <p>anecdote service not available due to problems in server</p>
+  }
+
+  if (result.isLoading) {
+    return <h3>Loading data ...</h3>
+  }
 
   return (
     <div>
@@ -22,7 +31,7 @@ const App = () => {
       <Notification />
       <AnecdoteForm />
     
-      {anecdotes.map(anecdote =>
+      { result.data.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
